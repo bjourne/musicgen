3a89392aee17cce5c1e645767269166f7be19f10
Björn Lindqvist <bjourne@gmail.com>
2020-04-08 12:16:35 +0200

    tests

--- tests/test_formats_modules.py
+++ tests/test_formats_modules.py
@@ -7,3 +7,11 @@ TEST_PATH = Path() / 'tests' / 'mods'
 def test_rows_to_string():
     mod = load(TEST_PATH / 'entity.mod')
     str = rows_to_string(mod.patterns[0].rows)
+    assert len(str) == 64 * (10 * 4 + 7) - 1
+
+    assert mod.patterns[0].rows[0][0].period == 509
+
+def test_load_samples():
+    mod = load(TEST_PATH / 'entity.mod')
+    samples = load_samples(mod)
+    assert samples[13].repeat_len == 0
