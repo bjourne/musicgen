6f0fb9fbfdebf9c2a9e45b08eff8ace1763b29c2
Björn Lindqvist <bjourne@gmail.com>
2020-06-12 05:16:31 +0200

    manual training loop maybe actually works

--- musicgen/tf_utils.py
+++ musicgen/tf_utils.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2020 Björn Lindqvist <bjourne@gmail.com>
 from musicgen.utils import SP
 from os import environ
-from tensorflow import constant, get_logger, int32
 from tensorflow.data import Dataset
 from tensorflow.config import (experimental_connect_to_cluster,
                                list_logical_devices)
@@ -10,23 +9,28 @@ from tensorflow.distribute.experimental import TPUStrategy
 from tensorflow.keras.utils import Sequence
 from tensorflow.tpu.experimental import initialize_tpu_system
 import numpy as np
+import tensorflow as tf
 
 def initialize_tpus():
+    SP.header('INITIALIZING TPUS')
     tpu_addr = environ.get('COLAB_TPU_ADDR')
     if not tpu_addr:
-        SP.print('TPU not configured.')
+        SP.print('TPU not found.')
+        SP.leave()
         return None
-    SP.print('Connecting to TPU at %s.' % tpu_addr)
+    SP.print('TPU address: %s' % tpu_addr)
     resolver = TPUClusterResolver('grpc://' + tpu_addr)
     experimental_connect_to_cluster(resolver)
     initialize_tpu_system(resolver)
     devs = list_logical_devices('TPU')
     assert len(devs) > 0
-    SP.header('%d TPU DEVICES' % len(devs))
+    SP.header('%d TPUS' % len(devs))
     for dev in devs:
         SP.print(dev)
     SP.leave()
     strategy = TPUStrategy(resolver)
+    SP.print('%d synced replicas.' % strategy.num_replicas_in_sync)
+    SP.leave()
     return strategy
 
 def sequence_to_batched_dataset(seq, seq_len, batch_size):
@@ -37,7 +41,7 @@ def sequence_to_batched_dataset(seq, seq_len, batch_size):
         return win.batch(seq_len + 1, drop_remainder = True)
     SP.print('Length %d, seq_len %d, batch_size %d.'
              % (len(seq), seq_len, batch_size))
-    source = constant(seq, dtype = int32)
+    source = tf.constant(seq, dtype = tf.int32)
     return Dataset    \
         .from_tensor_slices(source) \
         .window(seq_len + 1, stride, drop_remainder = True) \
@@ -79,3 +83,8 @@ def generate_sequences(model, temps, seed, length,
     SP.leave()
     return [[int(preds[j][i]) for j in range(length)]
             for i in range(batch_size)]
+
+if __name__ == '__main__':
+    a = np.arange(100)
+    for e in sequence_to_batched_dataset(a, 5, 2):
+        print(e)
--- tools/train-poly-model.py
+++ tools/train-poly-model.py
@@ -11,7 +11,8 @@ Options:
 """
 from docopt import docopt
 from musicgen.code_utils import code_to_string
-from musicgen.pcode import (EOS_SILENCE, INSN_SILENCE,
+from musicgen.pcode import (EOS_SILENCE,
+                            INSN_SILENCE,
                             load_corpus, load_mod_file,
                             pcode_to_midi_file)
 from musicgen.utils import (SP, analyze_code,
@@ -20,27 +21,30 @@ from musicgen.utils import (SP, analyze_code,
 from musicgen.tf_utils import (generate_sequences,
                                initialize_tpus,
                                sequence_to_batched_dataset)
-from os import listdir
 from pathlib import Path
 from random import randrange, shuffle
-from tensorflow import constant, get_logger, int32
 from tensorflow.data import Dataset
+from tensorflow.nn import compute_average_loss
 from tensorflow.keras import *
+from tensorflow.keras import losses
+from tensorflow.keras import metrics
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.layers import *
 from tensorflow.keras.optimizers import *
+from time import time
 import numpy as np
+import tensorflow as tf
 
 # Hyperparameters not from the command line here.
 class ExperimentParameters:
     BATCH_SIZE = 32
-    EPOCHS = 100
+    EPOCHS = 108
     LEARNING_RATE = 0.005
-    EMBEDDING_DIM = 32
-    LSTM1_UNITS = 128
-    LSTM2_UNITS = 128
-    DROPOUT = 0.2
-    SEQ_LEN = 128
+    EMBEDDING_DIM = 100
+    LSTM1_UNITS = 512
+    LSTM2_UNITS = 512
+    DROPOUT = 0.5
+    SEQ_LEN = 256
 
     def __init__(self, output_path, relative_pitches):
         self.output_path = output_path
@@ -48,7 +52,7 @@ class ExperimentParameters:
         self.prefix = 'pcode'
 
     def weights_path(self):
-        fmt = '%s_weights-%03d-%03d-%.5f-%02d-%03d-%03d-%.2f-%s.h5'
+        fmt = '%s_weights-%03d-%03d-%.5f-%02d-%04d-%04d-%.2f-%s.h5'
         args = (self.prefix,
                 self.BATCH_SIZE,
                 self.EPOCHS,
@@ -78,12 +82,13 @@ class ExperimentParameters:
             SP.print('%-22s: %5s' % (param, value))
         SP.leave()
 
-def lstm_model(params, vocab_size, batch_size, stateful):
+def create_model(params, vocab_size, batch_size, stateful):
     return Sequential([
         Embedding(
             input_dim = vocab_size,
             output_dim = params.EMBEDDING_DIM,
             batch_input_shape = [batch_size, None]),
+        Dropout(0.1),
         LSTM(
             params.LSTM1_UNITS,
             stateful = stateful,
@@ -99,10 +104,11 @@ def lstm_model(params, vocab_size, batch_size, stateful):
     ])
 
 def create_training_model(params, vocab_size):
-    model = lstm_model(params, vocab_size, None, False)
-    opt1 = RMSprop(learning_rate = params.LEARNING_RATE)
+    model = create_model(params, vocab_size, None, False)
+    #opt = SGD(learning_rate = 0.01)
+    opt = RMSprop(learning_rate = params.LEARNING_RATE)
     model.compile(
-        optimizer = opt1,
+        optimizer = opt,
         loss = 'sparse_categorical_crossentropy',
         metrics = ['sparse_categorical_accuracy'])
     return model
@@ -145,16 +151,115 @@ def do_train(train, validate, vocab_size, params):
                         verbose = 2)
     print(history)
 
-def do_predict(seq, ix2ch, ch2ix, temperatures, params):
-    batch_size = len(temperatures)
+# def get_dataset(raw_dataset, batch_size, seq_len):
+#     stride = seq_len - 1
+#     def split_input_target(chunk):
+#         return chunk[:-1], chunk[1:]
+#     def flatten_window(win):
+#         return win.batch(seq_len + 1, drop_remainder = True)
+#     source = tf.constant(raw_dataset, dtype = tf.int32)
+#     return Dataset \
+#         .from_tensor_slices(source) \
+#         .window(seq_len + 1, stride, drop_remainder = True) \
+#         .flat_map(flatten_window) \
+#         .map(split_input_target) \
+#         .shuffle(10000) \
+#         .batch(batch_size, drop_remainder = True)
+
+# Very strange code!
+def distribute_dataset(strategy, ds, batch_size_per_replica, seq_len):
+    def dataset_fn(ctx):
+        return sequence_to_batched_dataset(
+            ds, seq_len, batch_size_per_replica)
+    return strategy.experimental_distribute_datasets_from_function(
+        dataset_fn)
+
+@tf.function
+def train_epoch(model, opt, strategy, batch_size, ds, loss_var, acc_var):
+    def step_fn(X, Y):
+        with tf.GradientTape() as tape:
+            Y_hat = model(X, training = True)
+            loss = losses.sparse_categorical_crossentropy(Y, Y_hat)
+            loss = tf.reduce_sum(loss) / batch_size
+        grads = tape.gradient(loss, model.trainable_variables)
+        gvs = list(zip(grads, model.trainable_variables))
+        capped_gvs = [(tf.clip_by_norm(g, 0.15), v) for (g, v) in gvs]
+        opt.apply_gradients(capped_gvs)
+        loss_var.update_state(Y, Y_hat)
+        acc_var.update_state(Y, Y_hat)
+
+    for X, Y in ds:
+        strategy.run(step_fn, args = (X, Y))
+
+@tf.function
+def evaluate_epoch(model, strategy, batch_size, ds, loss_var, acc_var):
+    def step_fn(X, Y):
+        Y_hat = model(X, training = False)
+        loss = losses.sparse_categorical_crossentropy(Y, Y_hat)
+        loss = tf.reduce_sum(loss) / batch_size
+        loss_var.update_state(Y, Y_hat)
+        acc_var.update_state(Y, Y_hat)
+
+    for X, Y in ds:
+        strategy.run(step_fn, args = (X, Y))
+
+def do_train3(train, valid, vocab_size, params):
+    strategy = initialize_tpus()
+    with strategy.scope():
+        model = create_model(params, vocab_size, None, False)
+        model.summary()
+        opt = RMSprop(learning_rate = params.LEARNING_RATE)
+        train_loss = metrics.SparseCategoricalCrossentropy()
+        train_acc = metrics.SparseCategoricalAccuracy()
+        valid_loss = metrics.SparseCategoricalCrossentropy()
+        valid_acc = metrics.SparseCategoricalAccuracy()
+    observers = (train_loss, train_acc, valid_loss, valid_acc)
+
+    batch_size = params.BATCH_SIZE
+    seq_len = params.SEQ_LEN
+    batch_size_per_replica = batch_size // strategy.num_replicas_in_sync
+
+    train_ds = distribute_dataset(strategy, train,
+                                  batch_size, seq_len)
+    valid_ds = distribute_dataset(strategy, valid,
+                                  batch_size, seq_len)
+
+    fmt = '\-> %3d / %3d - %4db - %3ds - %.4f / %.4f - %.2f / %.2f'
+    last_time = time()
+    last_n_steps = 0
+    n_epochs = params.EPOCHS
+    for i in range(n_epochs):
+        start = time()
+        train_epoch(model, opt, strategy, batch_size,
+                    train_ds, train_loss, train_acc)
+        evaluate_epoch(model, strategy, batch_size,
+                       valid_ds, valid_loss, valid_acc)
+
+        new_time = time()
+        new_n_steps = opt.iterations.numpy()
+        time_delta = new_time - last_time
+        n_steps_delta = new_n_steps - last_n_steps
+        results = [obs.result() for obs in observers]
+        args = (i + 1, n_epochs, n_steps_delta, time_delta,
+                results[0], results[2], results[1], results[3])
+        SP.print(fmt % args)
+        last_time = new_time
+        last_n_steps = new_n_steps
+        for obs in observers:
+            obs.reset_states()
+
+def do_predict(seq, ix2ch, ch2ix, temps, params):
+    batch_size = len(temps)
     SP.header('%d PREDICTIONS' % batch_size)
-    model = lstm_model(params, len(ix2ch), batch_size, True)
+    model = create_model(params, len(ix2ch), batch_size, True)
     model.load_weights(str(params.weights_path()))
     model.reset_states()
 
+    seed_len = 128
+
     while True:
-        idx = randrange(len(seq) - params.SEQ_LEN)
-        seed = seq[idx:idx + params.SEQ_LEN]
+        idx = randrange(len(seq) - seed_len)
+        seed = seq[idx:idx + seed_len]
         if not list(find_subseq(seed.tolist(), EOS_SILENCE)):
             break
         SP.print('EOS_SILENCE in seed, regenerating.')
@@ -162,22 +267,23 @@ def do_predict(seq, ix2ch, ch2ix, temperatures, params):
     SP.print('Seed %s.' % seed_string)
 
     seed = np.repeat(np.expand_dims(seed, 0), batch_size, axis = 0)
-    seqs = generate_sequences(model, temperatures, seed, 500)
+    seqs = generate_sequences(model, temps, seed, 500, [])
     # Two bars of silence
     join = np.array([ch2ix[(INSN_SILENCE, 16)]] * 2)
     join = np.repeat(np.expand_dims(join, 0), batch_size, axis = 0)
     seqs = np.hstack((seed, join, seqs))
 
     seqs = [[ix2ch[ix] for ix in seq] for seq in seqs]
-    file_name_fmt = 'tpu-test-%s-%02d.mid'
-    for i, seq in enumerate(seqs):
-        args = params.relative_pitches, i
+    file_name_fmt = '%s_output-%s-%.2f.mid'
+    for temp, seq in zip(temps, seqs):
+        args = (params.prefix, params.relative_pitches, temp)
         file_name = file_name_fmt % args
         file_path = params.output_path / file_name
         pcode_to_midi_file(seq, file_path, params.relative_pitches)
     SP.leave()
 
 def main():
+    # Prologue
     args = docopt(__doc__, version = 'Train LSTM 1.0')
     SP.enabled = args['--verbose']
     output_path = Path(args['<corpus-path>'])
@@ -193,15 +299,15 @@ def main():
     analyze_code(ix2ch, seq)
     params = ExperimentParameters(output_path, relative_pitches)
     params.print()
-
     vocab_size = len(ix2ch)
 
     # Split data
     train, validate, test = split_train_validate_test(seq, 0.8, 0.1)
 
     # Run training and prediction.
-    do_train(train, validate, vocab_size, params)
-    do_predict(test, ix2ch, ch2ix, [0.5, 0.8, 1.0, 1.2, 1.5], params)
+    do_train3(train, validate, vocab_size, params)
+    # do_train(train, validate, vocab_size, params)
+    # do_predict(test, ix2ch, ch2ix, [0.5, 0.8, 1.0, 1.2, 1.5], params)
 
 if __name__ == '__main__':
     main()
