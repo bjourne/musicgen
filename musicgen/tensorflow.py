1f1ebbd9f93e81613fcd1ad4f81cc88e316bc258
Björn Lindqvist <bjourne@gmail.com>
2020-07-03 12:35:50 +0200

    also generating using a transformer, not 100% done yet

--- musicgen/tensorflow.py
+++ musicgen/tensorflow.py
@@ -8,10 +8,16 @@ from tensorflow.config import (experimental_connect_to_cluster,
 from tensorflow.distribute import OneDeviceStrategy
 from tensorflow.distribute.cluster_resolver import TPUClusterResolver
 from tensorflow.distribute.experimental import TPUStrategy
-from tensorflow.keras.utils import Sequence
+from tensorflow.keras import *
+from tensorflow.keras.initializers import RandomUniform
+from tensorflow.keras.layers import *
+from tensorflow.nn import softmax
 from tensorflow.tpu.experimental import initialize_tpu_system
 import numpy as np
 import tensorflow as tf
+
+# Transformer epsilon. Unsure if this needs to be configurable.
+TRANSF_EPS = 1e-6
 
 def select_strategy():
     SP.header('SELECTING STRATEGY')
@@ -85,3 +91,121 @@ def generate_sequences(model, temps, seed, length, excluded):
     SP.leave()
     return [[int(preds[j][i]) for j in range(length)]
             for i in range(batch_size)]
+
+def compute_and_apply_gradients(model, x, y):
+    with tf.GradientTape() as tape:
+        y_hat = model(x, training = True)
+        loss = model.compiled_loss(y, y_hat,
+                                   regularization_losses = model.losses)
+    vars = model.trainable_variables
+    grads = tape.gradient(loss, vars)
+    grads, _ = tf.clip_by_global_norm(grads, 15)
+    model.optimizer.apply_gradients(zip(grads, vars))
+    return y_hat
+
+class MyModel(Model):
+    def train_step(self, data):
+        x, y = data
+        y_hat = compute_and_apply_gradients(self, x, y)
+        self.compiled_metrics.update_state(y, y_hat)
+        return {m.name: m.result() for m in self.metrics}
+
+def lstm_model(vocab_size, emb_size,
+               lstm1_units, lstm2_units,
+               dropout, rec_dropout,
+               stateful, batch_size):
+    inp = Input(
+        shape = (None,),
+        batch_size = batch_size,
+        dtype = tf.int32)
+    embedding = Embedding(
+        input_dim = vocab_size,
+        output_dim = emb_size)
+    lstm1 = LSTM(
+        lstm1_units,
+        stateful = stateful,
+        return_sequences = True,
+        dropout = dropout,
+        recurrent_dropout = rec_dropout)
+    lstm2 = LSTM(
+        lstm2_units,
+        stateful = stateful,
+        return_sequences = True,
+        dropout = dropout,
+        recurrent_dropout = rec_dropout)
+    time_dist = TimeDistributed(Dense(vocab_size))
+    out = time_dist(lstm2(lstm1(embedding(inp))))
+    return MyModel(inputs = [inp], outputs = [out])
+
+def split_heads(inp, n_heads, depth, batch_size):
+    inp = tf.reshape(inp, (batch_size, -1, n_heads, depth))
+    return tf.transpose(inp, perm = [0, 2, 1, 3])
+
+def transformer_model(vocab_size, d_model, ffn_units, dropout,
+                      n_layers, n_heads):
+
+    # Variables.
+    depth = d_model // n_heads
+    d_model_f32 = tf.cast(d_model, tf.float32)
+    depth_f32 = tf.cast(depth, tf.float32)
+
+    # Setup the position encoding
+    pos = tf.range(5000, dtype = tf.float32)[:, tf.newaxis]
+    i = tf.range(d_model, dtype = tf.float32)[tf.newaxis, :]
+    angle_rads = pos / tf.pow(10_000, (2 * (i // 2)) / d_model_f32)
+    sines = tf.math.sin(angle_rads[:, 0::2])
+    cosines = tf.math.cos(angle_rads[:, 1::2])
+    pos_encoding = tf.concat([sines, cosines], axis = -1)
+    pos_encoding = tf.expand_dims(pos_encoding, 0)
+
+    # Input and look-ahead mask.
+    inp = Input(shape = (None,))
+
+    random_uniform = RandomUniform(-0.1, 0.1)
+    x = Embedding(vocab_size, d_model,
+                  embeddings_initializer = random_uniform)(inp)
+    x *= tf.math.sqrt(d_model_f32)
+
+    # Important variables.
+    batch_size = tf.shape(x)[0]
+    seq_len = tf.shape(x)[1]
+
+    # Hopefully this is only calculated once?
+    x += pos_encoding[:, :seq_len, :]
+    x = Dropout(dropout)(x)
+
+    # Look-ahead mask
+    mask = 1 - tf.linalg.band_part(tf.ones((seq_len, seq_len)), -1, 0)
+
+    for _ in range(n_layers):
+        # Multihead attention part
+        wq = Dense(d_model)(x)
+        wk = Dense(d_model)(x)
+        wv = Dense(d_model)(x)
+
+        q = split_heads(wq, n_heads, depth, batch_size)
+        k = split_heads(wk, n_heads, depth, batch_size)
+        v = split_heads(wv, n_heads, depth, batch_size)
+
+        # Scaled dot product attention
+        matmul_qk = tf.matmul(q, k, transpose_b = True)
+        logits = matmul_qk / tf.math.sqrt(depth_f32) + mask * -1e9
+        weights = softmax(logits, axis = -1)
+        attn = tf.matmul(weights, v)
+
+        # Pass through dense layer and normalize.
+        attn = tf.transpose(attn, perm = [0, 2, 1, 3])
+        attn = tf.reshape(attn, (batch_size, -1, d_model))
+        attn = Dense(d_model)(attn)
+        attn = Dropout(dropout)(attn)
+        x = LayerNormalization(epsilon = TRANSF_EPS)(x + attn)
+
+        # Point-wise feed-forward
+        ffn = Dense(ffn_units, activation = 'relu')(x)
+        ffn = Dropout(dropout)(ffn)
+        ffn = Dense(d_model)(ffn)
+        ffn = Dropout(dropout)(ffn)
+        x = LayerNormalization(epsilon = TRANSF_EPS)(x + ffn)
+
+    x = Dense(vocab_size, kernel_initializer = random_uniform)(x)
+    return Model(inputs = inp, outputs = x)
--- tools/model-trainer.py
+++ tools/model-trainer.py
@@ -4,15 +4,20 @@ Generative MIDI models trained on MOD files
 ===========================================
 
 Usage:
-    model-trainer.py [options] <code-type> ( train | generate )
+    model-trainer.py [options] <code-type> lstm ( train | generate )
         <corpus-path> --emb-size=<i> --batch-size=<i>
         --dropout=<f> --rec-dropout=<f>
-        --lstm1-units=<i> --lstm2-units=<i> --lr=<f>
-        --seq-len=<i> --epochs=<i>
+        --lstm1-units=<i> --lstm2-units=<i>
+    model-trainer.py [options]
+        <code-type> transformer ( train | generate ) <corpus-path>
+        --dropout=<f> --batch-size=<i>
 
 Options:
     -h --help              show this screen
     -v --verbose           print more output
+    --lr=<f>               learning rate
+    --epochs=<i>           epochs to train for
+    --seq-len=<i>          training sequence length
 """
 from os import environ
 environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -21,7 +26,8 @@ from collections import Counter, namedtuple
 from docopt import docopt
 from musicgen.code_utils import CODE_MIDI_MAPPING
 from musicgen.corpus import load_index
-from musicgen.generation import notes_to_audio_file
+from musicgen.generation import (notes_to_audio_file,
+                                 notes_to_midi_file)
 from musicgen.parser import UnsupportedModule, load_file
 from musicgen.pcode import (is_pcode_learnable,
                             mod_to_pcode,
@@ -32,16 +38,19 @@ from musicgen.scode import (mod_file_to_scode,
                             scode_to_midi_file,
                             scode_short_pause,
                             scode_long_pause)
-from musicgen.tensorflow import select_strategy, sequence_to_samples
+from musicgen.tensorflow import (lstm_model,
+                                 select_strategy,
+                                 sequence_to_samples,
+                                 transformer_model)
 from musicgen.utils import (SP, CharEncoder,
                             file_name_for_params,
-                            find_subseq, flatten, load_pickle_cache)
+                            find_subseq, flatten,
+                            load_pickle_cache,
+                            save_pickle)
 from pathlib import Path
 from random import randrange, shuffle
 from tensorflow.data import Dataset
-from tensorflow.keras import *
 from tensorflow.keras.callbacks import *
-from tensorflow.keras.layers import *
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras.optimizers import *
 from tensorflow.nn import softmax
@@ -51,28 +60,25 @@ from tqdm import trange
 import numpy as np
 import tensorflow as tf
 
-CodeInfo = namedtuple('CodeInfo', ['to_code_fn', 'to_notes_fn',
+CodeInfo = namedtuple('CodeInfo', ['to_code_fn',
                                    'short_pause', 'long_pause',
                                    'is_learnable_fn'])
+
 # TODO: Fix scode
 CODE_TYPES = {
     'pcode_abs' : CodeInfo(lambda m: mod_to_pcode(m, False),
-                           lambda c: pcode_to_notes(c, False),
                            pcode_short_pause(),
                            pcode_long_pause(),
                            is_pcode_learnable),
     'pcode_rel' : CodeInfo(lambda m: mod_to_pcode(m, True),
-                           lambda c: pcode_to_notes(c, False),
                            pcode_short_pause(),
                            pcode_long_pause(),
                            is_pcode_learnable),
     'scode_abs' : CodeInfo(lambda m: mod_file_to_scode(m, False),
-                           lambda c, fp: scode_to_midi_file(c, fp, False),
                            scode_short_pause(),
                            scode_long_pause(),
                            None),
     'scode_rel' : CodeInfo(lambda m: mod_file_to_scode(m, True),
-                           lambda c, fp: scode_to_midi_file(c, fp, True),
                            scode_short_pause(),
                            scode_long_pause(),
                            None)
@@ -118,73 +124,75 @@ def build_cache(path, shuffle_file, mods, info):
 
     return encoder, arrs
 
+class LSTMParams:
+    def __init__(self, docopt_args):
+        self.rec_dropout = float(docopt_args['--rec-dropout'])
+        self.emb_size = int(docopt_args['--emb-size'])
+        self.lstm1_units = int(docopt_args['--lstm1-units'])
+        self.lstm2_units = int(docopt_args['--lstm2-units'])
+
+    def as_file_name_part(self):
+        fmt = '%.2f-%03d-%04d-%04d'
+        args = (self.rec_dropout, self.emb_size,
+                self.lstm1_units, self.lstm2_units)
+        return fmt % args
+
+class TransformerParams:
+    def __init__(self, docopt_args):
+        pass
+
+    def as_file_name_part(self):
+        return ''
+
 class ModelParams:
     @classmethod
     def from_docopt_args(cls, args):
         code_type = args['<code-type>']
+        model_type = 'lstm' if args['lstm'] else 'transformer'
+
+        if model_type == 'lstm':
+            type_params = LSTMParams(args)
+        else:
+            type_params = TransformerParams(args)
+
         dropout = float(args['--dropout'])
-        rec_dropout = float(args['--rec-dropout'])
-        emb_size = int(args['--emb-size'])
         batch_size = int(args['--batch-size'])
-        lstm1_units = int(args['--lstm1-units'])
-        lstm2_units = int(args['--lstm2-units'])
         lr = float(args['--lr'])
         seq_len = int(args['--seq-len'])
         epochs = int(args['--epochs'])
-        return cls(code_type,
-                   dropout, rec_dropout,
-                   lstm1_units, lstm2_units,
-                   emb_size, batch_size,
-                   seq_len, epochs, lr)
+        return cls(code_type, model_type,
+                   dropout, batch_size, lr, seq_len, epochs,
+                   type_params)
 
-    def __init__(self, code_type,
-                 dropout, rec_dropout,
-                 lstm1_units, lstm2_units,
-                 emb_size, batch_size,
-                 seq_len, epochs, lr):
+    def __init__(self,
+                 code_type, model_type,
+                 dropout, batch_size, lr, seq_len, epochs,
+                 type_params):
         self.code_type = code_type
+        self.model_type = model_type
+
         self.dropout = dropout
-        self.rec_dropout = rec_dropout
-        self.lstm1_units = lstm1_units
-        self.lstm2_units = lstm2_units
-        self.emb_size = emb_size
         self.batch_size = batch_size
+        self.lr = lr
         self.seq_len = seq_len
         self.epochs = epochs
-        self.lr = lr
+        self.type_params = type_params
 
     def weights_file(self):
-        args = (self.code_type, self.epochs,
-                self.emb_size, self.batch_size,
-                self.dropout, self.rec_dropout,
-                self.lstm1_units, self.lstm2_units,
-                self.lr, self.seq_len)
-        fmt = 'weights_%s-%03d-%03d-%03d-%.2f-%.2f-%03d-%03d-%.5f-%03d.h5'
+        fmt = 'weights_%s_%s-%.2f-%03d-%.5f-%03d-%03d-%s.h5'
+        args = (self.code_type, self.model_type,
+                self.dropout, self.batch_size,
+                self.lr, self.seq_len, self.epochs,
+                self.type_params.as_file_name_part())
         return fmt % args
 
     def model(self, vocab_size, batch_size, stateful):
-        inp = Input(
-            shape = (None,),
-            batch_size = batch_size,
-            dtype = tf.int32)
-        embedding = Embedding(
-            input_dim = vocab_size,
-            output_dim = self.emb_size)
-        lstm1 = LSTM(
-            self.lstm1_units,
-            stateful = stateful,
-            return_sequences = True,
-            dropout = self.dropout,
-            recurrent_dropout = self.rec_dropout)
-        lstm2 = LSTM(
-            self.lstm2_units,
-            stateful = stateful,
-            return_sequences = True,
-            dropout = self.dropout,
-            recurrent_dropout = self.rec_dropout)
-        time_dist = TimeDistributed(Dense(vocab_size))
-        out = time_dist(lstm2(lstm1(embedding(inp))))
-        return MyModel(inputs = [inp], outputs = [out])
+        if self.model_type == 'transformer':
+            return transformer_model(vocab_size, 128, 2048, 0.2, 8, 16)
+        return lstm_model(vocab_size, self.emb_size,
+                          self.lstm1_units, self.lstm2_units,
+                          self.dropout, self.rec_dropout,
+                          stateful, batch_size)
 
 class TrainingData:
     def __init__(self, code_type):
@@ -248,6 +256,10 @@ class TrainingData:
         notes = self.info.to_notes_fn(code)
         notes_to_audio_file(notes, file_path, CODE_MIDI_MAPPING, True)
 
+    def code_to_pickle_file(self, seq, file_path):
+        code = self.encoder.decode_chars(seq)
+        save_pickle(file_path, code)
+
     def flatten(self):
         pause = self.encoder.encode_chars(self.info.long_pause, False)
         padded = []
@@ -260,24 +272,6 @@ class TrainingData:
     def to_samples(self, length):
         seq = self.flatten()
         return sequence_to_samples(seq, length)
-
-def compute_and_apply_gradients(model, x, y):
-    with tf.GradientTape() as tape:
-        y_hat = model(x, training = True)
-        loss = model.compiled_loss(y, y_hat,
-                                   regularization_losses = model.losses)
-    vars = model.trainable_variables
-    grads = tape.gradient(loss, vars)
-    grads, _ = tf.clip_by_global_norm(grads, 15)
-    model.optimizer.apply_gradients(zip(grads, vars))
-    return y_hat
-
-class MyModel(Model):
-    def train_step(self, data):
-        x, y = data
-        y_hat = compute_and_apply_gradients(self, x, y)
-        self.compiled_metrics.update_state(y, y_hat)
-        return {m.name: m.result() for m in self.metrics}
 
 def generate_sequences(model, temps, top_ps, seed, n_samples):
     SP.print('Priming the model with %d tokens.' % seed.shape[1])
@@ -322,19 +316,59 @@ def generate_sequences(model, temps, top_ps, seed, n_samples):
     return [[int(preds[j][i]) for j in range(n_samples)]
             for i in range(n_temps + n_top_ps)]
 
+def generate_sequences_transformer(model, temps, top_ps, seed, n_samples):
+    seed = np.array(seed, dtype = np.int32)
+    n_temps = len(temps)
+    n_top_ps = len(top_ps)
+    preds = []
+
+    SP.print('Predicting %d tokens.' % n_samples)
+    for _ in trange(n_samples, unit = 'preds', mininterval = 0.5):
+        y = model.predict(seed)
+        Ps = y[:, -1, :]
+        Ps = softmax(Ps).numpy()
+
+        ixs = []
+        for i in range(n_temps):
+            P = np.exp(np.log(Ps[i]) / temps[i])
+            P = P / P.sum()
+            ixs.append(np.random.choice(len(P), p = P))
+
+        # Then top-p sampling
+        for i in range(n_top_ps):
+            P = Ps[i + n_temps]
+            prob_ixs = np.argsort(-P)
+
+            PC = np.cumsum(P[prob_ixs])
+            top_n = len(PC[PC <= top_ps[i]]) + 1
+
+            # Clear the prob of those who didn't make it.
+            P[prob_ixs[top_n:]] = 0.0
+            P = P / P.sum()
+            ixs.append(np.random.choice(len(P), p = P))
+        ixs = np.array(ixs, dtype = np.int32)
+        preds.append(ixs)
+
+        seed = np.hstack((seed, np.expand_dims(ixs, 1)))
+        seed = np.roll(seed, -1, axis = 1)
+        seed[:,-1] = ixs
+    return [[int(preds[j][i]) for j in range(n_samples)]
+            for i in range(n_temps + n_top_ps)]
+
 def generate_music(temps, top_ps, data, path, params):
     n_temps = len(temps)
     n_top_ps = len(top_ps)
     n_preds = n_temps + n_top_ps
 
     # Often more than one full song - not great.
-    n_samples = 1200
+    n_samples = 100
     n_seed = 256
 
     SP.header('%d PREDICTIONS' % n_preds)
-    model = params.model(len(data.encoder.ix2ch), n_preds, True)
-    model.load_weights(str(path / params.weights_file()))
-    model.reset_states()
+    with select_strategy().scope():
+        model = params.model(len(data.encoder.ix2ch), n_preds, True)
+        model.load_weights(str(path / params.weights_file()))
+        model.reset_states()
     seq = data.flatten()
     long_pause = data.info.long_pause
     long_pause = data.encoder.encode_chars(long_pause, False)
@@ -355,7 +389,11 @@ def generate_music(temps, top_ps, data, path, params):
     SP.print('Seed %d+%d (%d unique tokens).' % (idx, n_seed, n_unique))
 
     seed = np.repeat(np.expand_dims(seed, 0), n_preds, axis = 0)
-    seqs = generate_sequences(model, temps, top_ps, seed, n_samples)
+    if params.model_type == 'transformer':
+        seqs = generate_sequences_transformer(model, temps, top_ps,
+                                              seed, n_samples)
+    else:
+        seqs = generate_sequences(model, temps, top_ps, seed, n_samples)
 
     # Add the original
     seqs.append(seq[idx + n_seed:idx + n_seed + n_samples])
@@ -363,31 +401,23 @@ def generate_music(temps, top_ps, data, path, params):
     seed = np.vstack((seed, seed[0]))
 
     # Cut half the seed because it is too long to listen to it all.
-    seed = seed[:,n_seed // 2:]
+    # seed = seed[:,n_seed // 2:]
 
     join = np.repeat(np.expand_dims(long_pause, 0), len(seqs), axis = 0)
-    print(seed.shape, join.shape, seqs.shape)
     seqs = np.hstack((seed, join, seqs))
 
-    for i in range(n_temps):
-        file_name = '%s-t%.2f.mp3' % (data.code_type, temps[i])
-        file_path = path / file_name
-        data.code_to_audio_file(seqs[i], file_path)
+    prefix = '%s-%07d' % (data.code_type, idx)
 
-    for i in range(n_top_ps):
-        file_name = '%s-p%.2f.mp3' % (data.code_type, top_ps[i])
-        file_path = path / file_name
-        data.code_to_audio_file(seqs[n_temps + i], file_path)
-
-    file_path = path / ('%s-orig.mp3' % data.code_type)
-    data.code_to_audio_file(seqs[-1], file_path)
+    file_names = ['%s-t%.3f.pickle' % (prefix, t) for t in temps]
+    file_names += ['%s-p%.3f.pickle' % (prefix, p) for p in top_ps]
+    file_names.append('%s-orig.pickle' % prefix)
+    for file_name, seq in zip(file_names, seqs):
+        data.code_to_pickle_file(seq, path / file_name)
     SP.leave()
 
 def train_model(train, valid, path, params):
     vocab_size = len(train.encoder.ix2ch)
-    strategy = select_strategy()
-    with strategy.scope():
-
+    with select_strategy().scope():
         model = params.model(vocab_size, None, False)
         optimizer = RMSprop(learning_rate = params.lr)
         loss_fn = SparseCategoricalCrossentropy(from_logits = True)
@@ -401,6 +431,8 @@ def train_model(train, valid, path, params):
     if weights_path.exists():
         SP.print('Loading weights from %s...' % weights_path)
         model.load_weights(str(weights_path))
+    else:
+        SP.print('Weights file %s not found.' % weights_path)
 
     cb_best = ModelCheckpoint(
         str(weights_path),
@@ -426,7 +458,7 @@ def train_model(train, valid, path, params):
 
 def main():
     # Prologue
-    args = docopt(__doc__, version = 'Train LSTM 1.0')
+    args = docopt(__doc__, version = 'Train MOD model 1.0')
     SP.enabled = args['--verbose']
     path = Path(args['<corpus-path>'])
     np.set_printoptions(linewidth = 160)
@@ -450,8 +482,8 @@ def main():
 
     weights_file = params.weights_file()
     if do_generate:
-        temps = [0.8, 0.9, 1.0, 1.05, 1.15]
-        top_ps = [0.75, 0.85, 0.9, 0.94, 0.98]
+        temps = [0.9, 1.0, 1.05, 1.10, 1.15]
+        top_ps = [0.9, 0.94, 0.98, 0.99, 0.999]
         generate_music(temps, top_ps, valid, path, params)
     else:
         train_model(train, valid, path, params)
