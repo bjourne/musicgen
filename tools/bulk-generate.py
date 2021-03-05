33e92c86dd930704a789cebe4c0530b4fae387f3
Björn Lindqvist <bjourne@gmail.com>
2021-03-05 21:46:02 +0100

    support for generating more than 32 clips

--- tools/bulk-generate.py
+++ tools/bulk-generate.py
@@ -29,6 +29,59 @@ from tensorflow.nn import softmax
 import numpy as np
 import tensorflow as tf
 
+def bulk_generate(g, root_path, offsets, td, n_prompt, n_generate):
+    # Pick the prompts
+    n_clips = len(offsets)
+    n_frag = n_prompt + n_generate
+    frags = np.array([pick_song_fragment(td, o, n_frag, True)[1]
+                      for o in offsets])
+    prompt, orig = frags[:,:n_prompt], frags[:,n_prompt:]
+
+    # Token(s) to avoid
+    end_ix = td.encoder.encode_char((INSN_END, 0), False)
+
+    # Generate tokens
+    skews = [g['sampling-method']] * len(offsets)
+    if g['network-type'] in ('lstm', 'gpt2', 'transformer'):
+        vocab_size = len(td.encoder.ix2ch)
+        model = load_generating_model(g, root_path, vocab_size, n_clips)
+        seqs, log_probs = generate_sequences(g, model, prompt, n_generate,
+                                             [end_ix], skews)
+    elif g['network-type'] == 'original':
+        seqs = orig
+        log_probs = [0] * n_clips
+    elif g['network-type'] == 'random':
+        ixs = [ix for ix in td.encoder.ix2ch if ix != end_ix]
+        seqs = [choices(ixs, k = n_generate) for _ in offsets]
+        seqs = np.array(generated)
+        log_probs = [0] * n_clips
+    else:
+        assert False
+
+    # Concatenate
+    seqs = np.hstack((prompt, seqs))
+
+    # Save generated code
+    output_path = root_path / 'bulk-generated'
+    output_path.mkdir(exist_ok = True)
+
+    fmt = '%010d-%s-%s-%s%.3f-%04d.pickle.gz'
+    skew_type_to_char = {
+        'top-p' : 'p',
+        'temperature' : 't',
+        'original' : 'o',
+        'random' : 'r'
+    }
+    for seq, offset, log_prob, skew in zip(seqs, offsets,
+                                           log_probs, skews):
+        skew_ch = skew_type_to_char[skew[0]]
+        args = (offset, g['code-type'], g['network-type'],
+                skew_ch, skew[1], -log_prob)
+        filename = fmt % args
+        file_path = output_path / filename
+        code = td.encoder.decode_chars(seq)
+        save_pickle(file_path, code)
+
 def main():
     # Prologue
     args = docopt(__doc__, version = 'Bulk generator 1.0')
@@ -58,51 +111,18 @@ def main():
     output_path = root_path / 'bulk-generated'
     output_path.mkdir(exist_ok = True)
 
-    schedule_name = 'schedule-%03d.pickle.gz' % n_clips
+    schedule_name = 'schedule-%04d.pickle.gz' % n_clips
     schedule_path = output_path / schedule_name
     def build_schedule():
         return [pick_song_fragment(td, 'random', n_frag, False)[0]
                 for _ in range(n_clips)]
     offsets = load_pickle_cache(schedule_path, build_schedule)
 
-    # Pick fragments from the training data
-    frags = np.array([pick_song_fragment(td, o, n_frag, True)[1]
-                      for o in offsets])
-    prompt, orig = frags[:,:n_prompt], frags[:,n_prompt:]
-
-    # Token(s) to avoid
-    end_ix = td.encoder.encode_char((INSN_END, 0), False)
-
-    # Generate tokens
-    skews = [g['sampling-method']] * n_clips
-    if g['network-type'] in ('lstm', 'gpt2', 'transformer'):
-        model = load_generating_model(g, root_path, vocab_size, n_clips)
-        seqs, log_probs = generate_sequences(g, model, prompt, n_generate,
-                                             [end_ix], skews)
-    elif g['network-type'] == 'original':
-        seqs = orig
-        log_probs = [0] * n_clips
-    elif g['network-type'] == 'random':
-        ixs = [ix for ix in td.encoder.ix2ch if ix != end_ix]
-        seqs = [choices(ixs, k = n_generate) for _ in offsets]
-        seqs = np.array(generated)
-        log_probs = [0] * n_clips
-    else:
-        assert False
-
-    # Concatenate
-    seqs = np.hstack((prompt, seqs))
-
-    # Save generated code
-    fmt = '%010d-%s-%s-%s%.3f-%04d.pickle.gz'
-    for seq, offset, log_prob, skew in zip(seqs, offsets,
-                                           log_probs, skews):
-        args = (offset, g['code-type'], g['network-type'],
-                skew[0][0], skew[1], -log_prob)
-        filename = fmt % args
-        file_path = output_path / filename
-        code = td.encoder.decode_chars(seq)
-        save_pickle(file_path, code)
+    # Need to split up the load since 32 is the maximum number of
+    # sequences that can be generated in parallel.
+    for i in range(0, len(offsets), 32):
+        job = offsets[i:i+32]
+        bulk_generate(g, root_path, job, td, n_prompt, n_generate)
 
 if __name__ == '__main__':
     main()
